5c744a5f192758fec4fe4a3343c6871ff209f612
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-03-21 18:03:57 +0100

    the simple move finction

--- src/world.js
+++ src/world.js
@@ -1,19 +1,12 @@
 
-function ConsActor(_pos, _actions, _type) { return { pos: _pos, actions: _actions, type : _type}; }
+// function ConsActor(_pos, _actions, _type) { return { pos: _pos, actions: _actions, type : _type}; }
 
 
-function ActorTypeGen(pos, _dx, _dy, _type){
-    return ConsActor( pos, (anActor, aWorld) => [_dx, _dy], _type);
-}
+// function ActorTypeGen(pos, _dx, _dy, _type){
+//     return ConsActor( pos, (anActor, aWorld) => [_dx, _dy], _type);
+// }
 
-// const ActorsList = {
-//     SimpleMonster : ActorTypeGen({x : 11, y : 1}, 3, 3, "Monster"),
-//     BigMonster : ActorTypeGen({x : 5, y : 7}, 2, 1, "Monster"),
-//     SimpleTower : ActorTypeGen({x : 10, y : 1}, 0, 0, "Tower"),
-//     MagicTower : ActorTypeGen({x : 5, y : 4}, 0, 0, "Tower"),
-// };
-
-
+//This is a list of all types of actors
 const ActorsTypeList = {
     SimpleMonster : {dx : 3, dy : 3, type : "Monster"},
     BigMonster : {dx : 1, dy : 1, type : "Monster"},
@@ -21,15 +14,25 @@ const ActorsTypeList = {
     MagicTower : {dx : 0, dy : 0, type : "Tower"},
     Floor : {dx : 0, dy : 0, type : "Floor"},
     River : {dx : 0, dy : 0, type : "River" },
+    Road : {dx : 0, dy : 0, type : "Road" },
+};
+
+const Actor = {
+    pos : {},
+    typeActor : {},
 };
 // const MapTexture = {
 //     Floor : ActorTypeGen({x : 11, y : 1}, 3, 3, "Monster"),
     
 // }
-/* function SimpleMove(anActor, aWorld){
+//This function return a possible place to move for the actor 
+function SimpleMove(anActor, aWorld){
     let x = anActor.pos.x, y = anActor.pos.y;
-    if (isEmptyPosition(aWorld[x])){
-
-    }
-} */
+    let dx = anActor.typeActor.dx , dy = anActor.typeActor.dy ;
+    if (isEmptyPosition(aWorld[x + dx ][y])) return [x + dx,y];
+    if (isEmptyPosition(aWorld[x + dx ][y + dy])) return [x + dx,y + dy];
+    if (isEmptyPosition(aWorld[x + dx ][y - dy])) return [x + dx,y - dy];
+    if (isEmptyPosition(aWorld[x ][y + dy])) return [x ,y + dy];
+    if (isEmptyPosition(aWorld[x ][y - dy])) return [x ,y - dy];
+}
 console.log(ActorsList.SimpleMonster);
