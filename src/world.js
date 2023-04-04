320e6e6631e0671f9d07fd9c0ff6f8ae77cec3dd
BOUHAJA Mohammed <Mohammed.Bouhaja@bordeaux-inp.fr>
2023-04-04 14:34:43 +0200

    game_loop files

--- src/world.js
+++ src/world.js
@@ -1,5 +1,5 @@
 
-import {Road} from './rand.js'
+import {Road} from './rand_road.js'
 import {create_simple_tower, create_magic_tower, enemies_in_attack_range, Tower_attacks} from './actors.js'
 import { available_position } from './movements.js';
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -228,63 +228,9 @@ test_GameMotor();
 
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////        LOOP             /////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function loop(){
-    
-    let world={
-        actors:[],
-        Width:15,
-        Height :10 ,
-        score:0,
-        Matrix:{}
-    };
-let start = Math.floor(world.Height/2)*world.Width;
-let end = start-1;
-    world=Road(initializeWorld(world),start,end);
 
-    world=create_simple_tower(Math.floor(world.Height/2)+2,11,world);
-    for(let i=0;i<2;i++){
-        if(i%6==0){
-            
-             world.actors.push({
-                pos:     { x: Math.floor(world.Height/2), y: 0 },
-                typeActor:ActorsTypeList.BigMonster
-
-            });
-            world.Matrix[Math.floor(world.Height/2)][0].ActorsTypeList=ActorsTypeList.BigMonster;
-        }
-        if(i%6==3){
-            {
-            
-                world.actors.push({
-                   pos:     { x: Math.floor(world.Height/2)+1, y: 0 },
-                   typeActor:ActorsTypeList.SimpleMonster
-   
-               });
-               world.Matrix[Math.floor(world.Height/2)+1][0].ActorsTypeList=ActorsTypeList.SimpleMonster;
-           }
-        }
-        
-    display(world);
-    console.log()
-	
-	for(let j=0;j<world.actors.length;j++){
-            var actor=world.actors[j]
-            let t=actor.typeActor.Movement(actor,world,actor.type);
-            let [a,b]=t
-            world.Matrix[world.actors[j].pos.x][world.actors[j].pos.y].typeActor=ActorsTypeList.Road;
-            world.Matrix[a][b].typeActor=world.actors[j].typeActor;
-            world.actors[j].pos={x:a,y:b};
-	}
-	
-    }
-}
- loop();
-
- export {ActorsTypeList}
+ export {ActorsTypeList, display, initializeWorld}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////           END           /////////////////////////////////////////////////////
