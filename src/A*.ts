6bad3391e6f53fa4354c5aa54e4249899a56bd2c
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-04-20 05:21:51 +0200

    debbuged A* file

--- src/A*.ts
+++ src/A*.ts
@@ -1,78 +1,78 @@
 // import * as W from '../src/world.js';
 
 export type Vertex = {
-    s : number; 
+  s : number; 
 }
-  
-export type Graph = {
-    mat: number[][];
-    size : number;
-  };
-  
-export type Arc = {
-    from: Vertex;
-    to: Vertex;
-    weight: number;
-  };
 
-  
+export type Graph = {
+  mat: number[][];
+  size : number;
+};
+
+export type Arc = {
+  from: Vertex;
+  to: Vertex;
+  weight: number;
+};
+
+
 function A_star(s: Vertex, t: Vertex, G: Graph): [number[], number[]] {
-    let [distances, parents] = initialization(s, G);
-    let GRIS = new Set([s]);
-    let NOIR = new Set<Vertex>();
-    // let distances : number[] = new Array(G.size);
-    // let parents: number[] = new Array(G.size);
-  
-    while (true) {
-      if (GRIS.size == 0) {
-        return [distances, parents];
-      }
-      let x = calculate_d_plus_h_min(GRIS, distances, G);
-      if (x == t) {
-        return [distances, parents];
-      }
-      let L = get_outgoing_arcs(x, G);
-      for (let i = 0; i < L.length; i++) {
-        relax(L[i], G, distances, parents, GRIS, NOIR);
-      }
-      color_node_black(x, GRIS, NOIR);
+  const [distances, parents] = initialization(s, G);
+  const GRIS = new Set([s]);
+  const NOIR = new Set<Vertex>();
+  // let distances : number[] = new Array(G.size);
+  // let parents: number[] = new Array(G.size);
+  // const bool : number = 1;
+  while (true) {
+    if (GRIS.size === 0) {
+      return [distances, parents];
     }
+    const x = calculate_d_plus_h_min(GRIS, distances, G);
+    if (x === t) {
+      return [distances, parents];
+    }
+    const L = get_outgoing_arcs(x, G);
+    for (let i = 0; i < L.length; i++) {
+      relax(L[i], G, distances, parents, GRIS, NOIR);
+    }
+    color_node_black(x, GRIS, NOIR);
   }
-  
+}
+
 function relax(e: Arc, G: Graph, d: number[], parent: number[], GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
-    let [x, y] = [e.from, e.to];
-    if (d[x.s] + e.weight < d[y.s]) {
-      d[y.s] = d[x.s] + e.weight;
-      parent[y.s] = x.s;
-      color_node_gray(y, GRIS, NOIR);
-    }
+  const [x, y] = [e.from, e.to];
+  if (d[x.s] + e.weight < d[y.s]) {
+    d[y.s] = d[x.s] + e.weight;
+    parent[y.s] = x.s;
+    color_node_gray(y, GRIS, NOIR);
   }
-  
+}
+
 function color_node_gray(y: Vertex, GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
-    if (NOIR.has(y)) {
-      NOIR.delete(y);
-      GRIS.add(y);
-    } else if (!GRIS.has(y)) {
-      GRIS.add(y);
-    }
+  if (NOIR.has(y)) {
+    NOIR.delete(y);
+    GRIS.add(y);
+  } else if (!GRIS.has(y)) {
+    GRIS.add(y);
   }
-  
+}
+
 function color_node_black(x: Vertex, GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
-    NOIR.add(x);
-    GRIS.delete(x);
-  }
-  
+  NOIR.add(x);
+  GRIS.delete(x);
+}
+
 function initialization(s: Vertex, G: Graph): [number[], number[]] {
-    let d : number[] = new Array(G.size);
-    let parent : number[] = new Array(G.size);
-    for (let v = 0; v < G.size; ++v) {
-      d[v] = Infinity;
-      parent[v] = -1;
-    }
-    d[s.s] = 0;
-    return [d, parent];
+  const d : number[] = new Array(G.size);
+  const parent : number[] = new Array(G.size);
+  for (let v = 0; v < G.size; ++v) {
+    d[v] = Infinity;
+    parent[v] = -1;
   }
-  
+  d[s.s] = 0;
+  return [d, parent];
+}
+
 // function calculate_d_plus_h_min(GRIS: Set<Vertex>, d: number[], G: Graph): Vertex {
 //     let x: Vertex | null = null;
 //     let min_val = Infinity;
@@ -85,59 +85,59 @@ function initialization(s: Vertex, G: Graph): [number[], number[]] {
 //     return x!;
 //   }
 function calculate_d_plus_h_min(GRIS: Set<Vertex>, d: number[], G: Graph): Vertex {
-    let x: Vertex | null = null;
-    let min_val = Infinity;
-    for (let iter = GRIS.values(), val = iter.next(); !val.done; val = iter.next()) {
-        let v = val.value;
-        if (d[v.s] + heuristic(v, G) < min_val) {
-            x = v;
-            min_val = d[v.s] + heuristic(v, G);
-        }
-    }
-    return x!;
-}
-  
-function get_outgoing_arcs(x: Vertex, G: Graph): Arc[] {
-    let arcs: Arc[] = [];
-    for (let i = 0; i < G.mat[x.s].length; i++) {
-      if (G.mat[x.s][i] != 0) {
-        arcs.push({
-          from: x,
-          to: { s: i },
-          weight: G.mat[x.s][i],
-        });
+  let x: Vertex | null = null;
+  let min_val = Infinity;
+  for (let iter = GRIS.values(), val = iter.next(); !val.done; val = iter.next()) {
+      let v = val.value;
+      if (d[v.s] + heuristic(v, G) < min_val) {
+          x = v;
+          min_val = d[v.s] + heuristic(v, G);
       }
-    }
-    return arcs;
   }
-  
+  return x!;
+}
+
+function get_outgoing_arcs(x: Vertex, G: Graph): Arc[] {
+  const arcs: Arc[] = [];
+  for (let i = 0; i < G.mat[x.s].length; i++) {
+    if (G.mat[x.s][i] !== 0) {
+      arcs.push({
+        from: x,
+        to: { s: i },
+        weight: G.mat[x.s][i],
+      });
+    }
+  }
+  return arcs;
+}
+
 function get_weight(e: Arc, G: Graph): number {
-    return G.mat[e.from.s][e.to.s];
+  return G.mat[e.from.s][e.to.s];
 }
 
 function heuristic(v: Vertex, G: Graph): number {
-    // implementation of the heuristic function goes here
-    return 0.5;
+  // implementation of the heuristic function goes here
+  return 0.5;
 }
 
 function Test_Astar(){
-    const G: Graph = {
-        mat: [
-          [0, 2, 0, 0, 5],
-          [2, 0, 1, 0, 0],
-          [0, 1, 0, 4, 0],
-          [0, 0, 4, 0, 3],
-          [5, 0, 0, 3, 0],
-        ],
-        size: 5,
-      };
-      
-      const s: Vertex = { s: 0 };
-      const t: Vertex = { s: 4 };
-      
-      const [distances, parents] = A_star(s, t, G);
-      
-      console.log(distances); // output: [0, 2, 3, 7, 5]
-      console.log(parents); // output: [-1, 0, 1, 2, 0]
+  const G: Graph = {
+      mat: [
+        [0, 2, 0, 0, 5],
+        [2, 0, 1, 0, 0],
+        [0, 1, 0, 4, 0],
+        [0, 0, 4, 0, 3],
+        [5, 0, 0, 3, 0],
+      ],
+      size: 5,
+    };
+    
+    const s: Vertex = { s: 0 };
+    const t: Vertex = { s: 4 };
+    
+    const [distances, parents] = A_star(s, t, G);
+    
+    console.log(distances); // output: [0, 2, 3, 7, 5]
+    console.log(parents); // output: [-1, 0, 1, 2, 0]
 }
 Test_Astar();
