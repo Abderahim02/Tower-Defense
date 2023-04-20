494063a32d5446631ed589575c4d65f999e50883
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-04-20 05:40:21 +0200

    renaming functions

--- src/A*.ts
+++ src/A*.ts
@@ -16,8 +16,8 @@ export type Arc = {
 };
 
 
-function A_star(s: Vertex, t: Vertex, G: Graph): [number[], number[]] {
-  const [distances, parents] = initialization(s, G);
+function Astar(s: Vertex, t: Vertex, G: Graph): [number[], number[]] {
+  const [distances, parents] = Initialization(s, G);
   const GRIS = new Set([s]);
   const NOIR = new Set<Vertex>();
   const b : number = 1;
@@ -25,29 +25,29 @@ function A_star(s: Vertex, t: Vertex, G: Graph): [number[], number[]] {
     if (GRIS.size === 0) {
       return [distances, parents];
     }
-    const x = calculate_d_plus_h_min(GRIS, distances, G);
+    const x = CalculateDplusHmin(GRIS, distances, G);
     if (x === t) {
       return [distances, parents];
     }
-    const L = get_outgoing_arcs(x, G);
+    const L = GetOutgoingArcs(x, G);
     for (let i = 0; i < L.length; i++) {
-      relax(L[i], G, distances, parents, GRIS, NOIR);
+      Relax(L[i], G, distances, parents, GRIS, NOIR);
     }
-    color_node_black(x, GRIS, NOIR);
+    ColorNodeBlack(x, GRIS, NOIR);
   }
   return [distances, parents];
 }
 
-function relax(e: Arc, G: Graph, d: number[], parent: number[], GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
+function Relax(e: Arc, G: Graph, d: number[], parent: number[], GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
   const [x, y] = [e.from, e.to];
   if (d[x.s] + e.weight < d[y.s]) {
     d[y.s] = d[x.s] + e.weight;
     parent[y.s] = x.s;
-    color_node_gray(y, GRIS, NOIR);
+    ColorNodeGray(y, GRIS, NOIR);
   }
 }
 
-function color_node_gray(y: Vertex, GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
+function ColorNodeGray(y: Vertex, GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
   if (NOIR.has(y)) {
     NOIR.delete(y);
     GRIS.add(y);
@@ -56,12 +56,12 @@ function color_node_gray(y: Vertex, GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
   }
 }
 
-function color_node_black(x: Vertex, GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
+function ColorNodeBlack(x: Vertex, GRIS: Set<Vertex>, NOIR: Set<Vertex>) {
   NOIR.add(x);
   GRIS.delete(x);
 }
 
-function initialization(s: Vertex, G: Graph): [number[], number[]] {
+function Initialization(s: Vertex, G: Graph): [number[], number[]] {
   const d : number[] = new Array(G.size);
   const parent : number[] = new Array(G.size);
   for (let v = 0; v < G.size; ++v) {
@@ -72,31 +72,31 @@ function initialization(s: Vertex, G: Graph): [number[], number[]] {
   return [d, parent];
 }
 
-// function calculate_d_plus_h_min(GRIS: Set<Vertex>, d: number[], G: Graph): Vertex {
+// function CalculateDplusHmin(GRIS: Set<Vertex>, d: number[], G: Graph): Vertex {
 //     let x: Vertex | null = null;
 //     let min_val = Infinity;
 //     for (let v of GRIS) {
-//       if (d[v.s] + heuristic(v, G) < min_val) {
+//       if (d[v.s] + Heuristic(v, G) < min_val) {
 //         x = v;
-//         min_val = d[v.s] + heuristic(v, G);
+//         min_val = d[v.s] + Heuristic(v, G);
 //       }
 //     }
 //     return x!;
 //   }
-function calculate_d_plus_h_min(GRIS: Set<Vertex>, d: number[], G: Graph): Vertex {
+function CalculateDplusHmin(GRIS: Set<Vertex>, d: number[], G: Graph): Vertex {
   let x: Vertex | null = null;
   let min_val = Infinity;
   for (let iter = GRIS.values(), val = iter.next(); !val.done; val = iter.next()) {
       const v = val.value;
-      if (d[v.s] + heuristic(v, G) < min_val) {
+      if (d[v.s] + Heuristic(v, G) < min_val) {
           x = v;
-          min_val = d[v.s] + heuristic(v, G);
+          min_val = d[v.s] + Heuristic(v, G);
       }
   }
   return x!;
 }
 
-function get_outgoing_arcs(x: Vertex, G: Graph): Arc[] {
+function GetOutgoingArcs(x: Vertex, G: Graph): Arc[] {
   const arcs: Arc[] = [];
   for (let i = 0; i < G.mat[x.s].length; i++) {
     if (G.mat[x.s][i] !== 0) {
@@ -114,12 +114,12 @@ function get_weight(e: Arc, G: Graph): number {
   return G.mat[e.from.s][e.to.s];
 }
 
-function heuristic(v: Vertex, G: Graph): number {
-  // implementation of the heuristic function goes here
+function Heuristic(v: Vertex, G: Graph): number {
+  // implementation of the Heuristic function goes here
   return 0.5;
 }
 
-function Test_Astar(){
+function TestAstar(){
   const G: Graph = {
       mat: [
         [0, 2, 0, 0, 5],
@@ -134,9 +134,9 @@ function Test_Astar(){
     const s: Vertex = { s: 0 };
     const t: Vertex = { s: 4 };
     
-    const [distances, parents] = A_star(s, t, G);
+    const [distances, parents] = Astar(s, t, G);
     
     console.log(distances); // output: [0, 2, 3, 7, 5]
     console.log(parents); // output: [-1, 0, 1, 2, 0]
 }
-Test_Astar();
+TestAstar();
