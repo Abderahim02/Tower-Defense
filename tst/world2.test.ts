759e587ddc2e9f81585234b88cd6cb8e9f38f827
Alloubane Moussa <moussa.alloubane@bordeaux-inp.fr>
2023-04-25 16:00:54 +0200

    updating tests 4 errors left

--- tst/world2.test.ts
+++ tst/world2.test.ts
@@ -3,7 +3,7 @@ import * as A from '../src/actors.js';
 import * as M from '../src/movements.js';
 import * as R from '../src/rand_road.js';
 
-//all world creatiion functions are to be modified after ts trandformation 
+//all world creatiion functions are to be modified after ts transformation 
 
 describe('world test suite', () => {
 
@@ -43,7 +43,7 @@ describe('mouvement test suite', () => {
             };
             console.log(W.ActorsTypeList.BigMonster.Type);
             // console.log(M.SimpleMove(w.Matrix[1][2],w,W.ActorsTypeList.BigMonster.Type));
-        expect(M.SimpleMove(w.Matrix[1][2],w,W.ActorsTypeList.BigMonster.Type)).toBe([1,2]);
+        expect(M.SimpleMove(w.Matrix[1][2],w,W.ActorsTypeList.BigMonster.Type)).toStrictEqual([1,2]);
     });
 });
 
@@ -112,4 +112,222 @@ describe('a phase test ', () => {
                 expect(world.Matrix[act.aMove.NewPos.x][act.aMove.NewPos.y].AnActor.Type).toBe(act.AnActorInfos.AnActor.Type);
             }     
     });
+});
+//test for CreateMagicTower
+describe('CreateMagicTower', () => {
+    let world=W.CreateWorld(10, 10);
+    world = W.initializeWorld(world);
+    
+    
+    test('should add MagicTower to world matrix if position is available', () => {
+      const i = 3;
+      const j = 5;
+      
+      const result = A.CreateMagicTower(i, j, world);
+      
+      expect(result.Matrix[i][j]).toEqual({
+        Pos: { x: i, y: j },
+        AnActor: W.ActorsTypeList.MagicTower,
+      });
+    });
+    
+    test('should not add MagicTower to world matrix if position is not available', () => {
+      const i = 7;
+      const j = 2;
+      
+      const result = A.CreateMagicTower(i, j, world);
+      
+      expect(result).toEqual(world);
+    });
+  });
+//test for CreateSimpleTower
+describe('CreateSimpleTower', () => {
+    let world=W.CreateWorld(10, 10);
+    world = W.initializeWorld(world);
+    
+    test('should add SimpleTower to world matrix if position is available', () => {
+      const i = 3;
+      const j = 5;
+      
+      const result = A.CreateSimpleTower(i, j, world);
+      
+      expect(result.Matrix[i][j]).toEqual({
+        Pos: { x: i, y: j },
+        AnActor: W.ActorsTypeList.SimpleTower,
+      });
+    });
+    
+    test('should not add SimpleTower to world matrix if position is not available', () => {
+      const i = 7;
+      const j = 2;
+      
+      const result = A.CreateSimpleTower(i, j, world);
+      
+      expect(result).toEqual(world);
+    });
+  }
+    );
+//test Enemiesinattackrange
+
+describe('EnemiesInAttackRange', () => {
+    let world=W.CreateWorld(3, 3);
+    world = W.initializeWorld(world);
+
+  beforeEach(() => {
+    world = {
+      Matrix: [
+        [
+          { Pos: { x: 0, y: 0 }, AnActor: W.ActorsTypeList.Floor },
+          { Pos: { x: 0, y: 1 }, AnActor: W.ActorsTypeList.Floor },
+          { Pos: { x: 0, y: 2 }, AnActor: W.ActorsTypeList.Floor },
+        ],
+        [
+          { Pos: { x: 1, y: 0 }, AnActor: W.ActorsTypeList.Floor },
+          { Pos: { x: 1, y: 1 }, AnActor: W.ActorsTypeList.BigMonster },
+          { Pos: { x: 1, y: 2 }, AnActor:{Move: W.noMove, Type : "SimpleTower", Color : "\x1b[48;2;34;139;34m🏯\x1b[0m", HitPoints : 0, Cost : 1000,gain : 0, Damage: 1, AttackRange : 1}, },
+        ],
+        [
+          { Pos: { x: 2, y: 0 }, AnActor: W.ActorsTypeList.Floor },
+          { Pos: { x: 2, y: 1 }, AnActor: W.ActorsTypeList.Floor },
+          { Pos: { x: 2, y: 2 }, AnActor: W.ActorsTypeList.Floor },
+        ],
+      ],
+        Actors: [],
+        Width: 3,
+        Height: 3,
+        Score: 0,
+        Towers: [],
+        
+    };
+  });
+
+  it('should return an empty array if no enemies are in attack range', () => {
+    const i = 0;
+    const j = 0;
+    const result = A.EnemiesInAttackRange(i, j, world);
+    expect(result).toEqual([]);
+  });
+
+  it('should return an array of enemies in attack range', () => {
+    const i = 1;
+    const j = 2;
+    const result = A.EnemiesInAttackRange(i, j, world);
+    expect(result).toEqual([{ x: 1, y: 1 }]);
+  });
+
+  it('should handle attack range greater than 1', () => {
+    const i = 1;
+    const j = 2;
+    world.Matrix[0][0].AnActor.Type =  W.ActorsTypeList.BigMonster.Type ;
+    world.Matrix[2][2].AnActor.Type = W.ActorsTypeList.BigMonster.Type ;
+    const result = A.EnemiesInAttackRange(i, j, world);
+    console.log(result);
+    expect(result).toEqual([{ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 2 }]);
+  });
+});
+//test for TowerAttacks
+describe('TowerAttacks', () => {
+    let world=W.CreateWorld(10, 10);
+    world = W.initializeWorld(world);
+
+  test('should damage an enemy within range and increase the score', () => {
+    const tower = {
+      Pos: { x: 3, y: 3 },
+      AnActor: W.ActorsTypeList.MagicTower,
+    };
+    const enemy = {
+      Pos: { x: 5, y: 5 },
+      AnActor: W.ActorsTypeList.BigMonster,
+    };
+    world.Matrix[tower.Pos.x][tower.Pos.y] = tower;
+    world.Matrix[enemy.Pos.x][enemy.Pos.y] = enemy;
+
+    jest.spyOn(global.Math, 'floor').mockReturnValue(0);
+
+    world = A.TowersAttacks(world);
+
+    expect(world.Matrix[enemy.Pos.x][enemy.Pos.y].AnActor.HitPoints).toBe(90);
+    expect(world.Score).toBe(1);
+
+   // global.Math.floor.mockRestore();
+  });
+
+  test('should not damage any enemy if none are within range', () => {
+    const tower = {
+      Pos: { x: 3, y: 3 },
+      AnActor: W.ActorsTypeList.MagicTower,
+    };
+    world.Matrix[tower.Pos.x][tower.Pos.y] = tower;
+
+   // jest.spyOn(A.EnemiesInAttackRange, 'mockReturnValue').mockReturnValue([]);
+
+    world = A.TowersAttacks(world);
+
+    expect(world.Score).toBe(0);
+
+   // EnemiesInAttackRange.mockRestore();
+  });
+});
+
+//testing for function display 
+
+describe('display', () => {
+  let mockWorld = W.CreateWorld(2, 2);
+    mockWorld = W.initializeWorld(mockWorld);
+    beforeEach(() => {
+    mockWorld = {
+      Width: 2,
+      Height: 2,
+      Score: 0,
+      Matrix: [
+        [
+          {
+            Pos: { x: 0, y: 0 },
+            AnActor: W.ActorsTypeList.SimpleMonster ,
+          },
+          {Pos: { x: 0, y: 1 },
+            AnActor: W.ActorsTypeList.SimpleTower ,
+          },
+        ],
+        [
+          {Pos: { x: 1, y: 0 },
+            AnActor: W.ActorsTypeList.Road ,
+          },
+          {Pos  : { x: 1, y: 1 },
+            AnActor: W.ActorsTypeList.Tree ,
+          },
+        ],
+      ],
+      Actors: [],
+      Towers: [],
+    };
+    });
+  ;
+
+  it('should display the world with correct colors', () => {
+    const mockConsoleLog = jest.spyOn(console, 'log').mockImplementation();
+    W.display(mockWorld);
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      expect.stringContaining(W.ActorsTypeList.SimpleMonster.Color)
+    );
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      expect.stringContaining(W.ActorsTypeList.SimpleTower.Color)
+    );
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      expect.stringContaining(W.ActorsTypeList.Road.Color)
+    );
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      expect.stringContaining(W.ActorsTypeList.Tree.Color)
+    );
+    mockConsoleLog.mockRestore();
+  });
+
+  it('should display the score', () => {
+    const mockConsoleLog = jest.spyOn(console, 'log').mockImplementation();
+    W.display(mockWorld);
+    expect(mockConsoleLog).toHaveBeenCalledWith(
+      expect.stringContaining(`Score : ${mockWorld.Score}`)
+    );
+    mockConsoleLog.mockRestore();
+  });
 });
